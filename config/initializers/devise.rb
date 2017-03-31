613791667848ab5567088d1e1a2ccf4619a3ca44
mariamEraQi <mariam.eleraki@gmail.com>
2017-03-31 05:56:13 +0200

    sign in using facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -26,6 +26,20 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
+  config.case_insensitive_keys = [ :email ]
+  config.strip_whitespace_keys = [ :email ]
+  config.skip_session_storage = [:http_auth]
+  config.stretches = Rails.env.test? ? 1 : 10
+  config.reconfirmable = true
+  config.expire_all_remember_me_on_sign_out = true
+  config.password_length = 8..128
+  config.reset_password_within = 6.hours
+  config.sign_out_via = :delete
+
+  config.omniauth :facebook, "1833501496910005",
+                             "b7fc544ce619c3fd56d9a8f7a39b3415",
+                             callback_url: "http://localhost:3000/users/auth/facebook/callback" 
+
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
