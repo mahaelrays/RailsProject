36b16bb79cfb06a3495b3b01d0835a912adc09af
fardoos <fardosg0@gmail.com>
2017-04-02 19:56:25 +0200

    update

--- app/models/friend.rb
+++
@@ -1,8 +0,0 @@
-class Friend < ApplicationRecord
-  # belongs_to :user
-  self.primary_keys = :user_id, :friend_id
-   belongs_to :user
-   belongs_to :user
-  #  has_many :statuses, :class_name => 'Friend', :foreign_key => [:user_id, :friend_id]
-  belongs_to :Friend, :foreign_key => [:user_id, :friend_id]
-end
